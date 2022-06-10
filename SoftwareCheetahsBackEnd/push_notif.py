6f1eca814d3e7bd8ecf5a9349f4f8f72401cd6ec
Mohamad Sajad Naghizadeh <naghizadeh.mdsd@gmail.com>
2022-06-10 16:17:23 +0430

    removed push notif

--- SoftwareCheetahsBackEnd/push_notif.py
+++ SoftwareCheetahsBackEnd/push_notif.py
@@ -13,23 +13,23 @@ def fcm_push_notifications(request):
 
     token = request.data['token']
 
-    try:
-        fcm_device, created = GCMDevice.objects.get_or_create(registration_id=token,
-                                                              cloud_message_type="FCM",
-                                                              user=request.user)
-
-        if 'message' not in request.data:
-            return Response(f"Message: None, BAD REQUEST!", status=status.HTTP_400_BAD_REQUEST)
-
-        if 'title' not in request.data:
-            return Response(f"Title: None, BAD REQUEST!", status=status.HTTP_400_BAD_REQUEST)
-
-        message = request.data['message']
-        title = request.data['title']
-
-        fcm_device.send_message(message=message, title=title)
-    except Exception as e:
-        return Response(f"ERROR: {e}", status=status.HTTP_400_BAD_REQUEST)
+    # try:
+    #     fcm_device, created = GCMDevice.objects.get_or_create(registration_id=token,
+    #                                                           cloud_message_type="FCM",
+    #                                                           user=request.user)
+    #
+    #     if 'message' not in request.data:
+    #         return Response(f"Message: None, BAD REQUEST!", status=status.HTTP_400_BAD_REQUEST)
+    #
+    #     if 'title' not in request.data:
+    #         return Response(f"Title: None, BAD REQUEST!", status=status.HTTP_400_BAD_REQUEST)
+    #
+    #     message = request.data['message']
+    #     title = request.data['title']
+    #
+    #     fcm_device.send_message(message=message, title=title)
+    # except Exception as e:
+    #     return Response(f"ERROR: {e}", status=status.HTTP_400_BAD_REQUEST)
 
     return Response(f"Push notification successfully done!", status=status.HTTP_200_OK)
 
@@ -42,11 +42,11 @@ def fcm_add_device(request):
 
     token = request.data['token']
 
-    try:
-        fcm_device = GCMDevice.objects.get(user=request.user)
-        fcm_device.registration_id = token
-        fcm_device.save()
-    except GCMDevice.DoesNotExist:
-        GCMDevice.objects.create(registration_id=token, cloud_message_type="FCM", user=request.user)
+    # try:
+    #     fcm_device = GCMDevice.objects.get(user=request.user)
+    #     fcm_device.registration_id = token
+    #     fcm_device.save()
+    # except GCMDevice.DoesNotExist:
+    #     GCMDevice.objects.create(registration_id=token, cloud_message_type="FCM", user=request.user)
 
     return Response(f"FCM device info updated!", status=status.HTTP_200_OK)
